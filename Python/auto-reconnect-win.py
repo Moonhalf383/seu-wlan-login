0bebf9f9686808c813dc04db239de5c899ad9ae0
Moonhalf383 <211029179@qq.com>
2026-03-05 13:31:05 +0800

    UI refined.

--- Python/auto-reconnect-win.py
+++ Python/auto-reconnect-win.py
@@ -4,12 +4,14 @@ import os
 import time
 import login
 
+
 def PingTest():
-    err = os.system('ping -4 -n 1 www.baidu.com')
+    err = os.system("ping -4 -n 1 www.baidu.com")
     if err:
         return False
     else:
         return True
+
 
 flag = False
 
@@ -18,16 +20,17 @@ while True:
         flag = False
     else:
         if flag:
-            print('断开连接……')
-            os.system('netsh wlan disconnect')
+            print("断开连接……")
+            os.system("netsh wlan disconnect")
             time.sleep(5)
-            print('尝试重新连接……')
-            err = os.system('netsh wlan connect name=seu-wlan')
+            print("尝试重新连接……")
+            err = os.system("netsh wlan connect name=seu-wlan")
             if not err:
                 time.sleep(30)
-                print('正在登录……')
+                print("正在登录……")
                 login.login()
         else:
-            print('发现网络异常')
+            print("发现网络异常")
             flag = True
     time.sleep(60)
+
